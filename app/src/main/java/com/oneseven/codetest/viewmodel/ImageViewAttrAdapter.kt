4120f39d67da443f0cb0d8ac812d0e29780d28a4
Daniel.Hsieh <r02944011@ntu.edu.tw>
2020-08-01 18:08:21 +0800

    load image into image view

---
+++ app/src/main/java/com/oneseven/codetest/viewmodel/ImageViewAttrAdapter.kt
@@ -0,0 +1,17 @@
+package com.oneseven.codetest.viewmodel
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.oneseven.codetest.R
+
+@BindingAdapter("android:imageUrl")
+fun ImageView.setImageUrl(url: String?) {
+    Log.d("ImageViewAttrAdapter", "url: $url")
+    Glide.with(context)
+        .load(url)
+        .placeholder(R.drawable.account_circle)
+        .error(R.drawable.account_circle)
+        .into(this)
+}
